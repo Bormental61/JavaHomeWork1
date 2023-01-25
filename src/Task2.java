9c67a4f1942051ddd046b54ba3e857eb5c209440
Bormental <tyranosaurus@yandex.ru>
2023-01-25 20:11:07 +0300

    Task 2 added

--- src/Task2.java
+++ src/Task2.java
@@ -1,2 +1,16 @@
+//2. Вывести все простые числа от 1 до 1000
 public class Task2 {
+    public static void main(String[] args) {
+        boolean s = true;
+        for (int i = 2; i <= 1000; i++) {
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    s = false;
+                    break;
+                }
+            }
+            if (s) System.out.println(i);
+            else s = true;
+        }
+    }
 }
